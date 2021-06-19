13c9689645881e4fea2366f1626b898e8b2c5fc3
Jason <AarsZach@yahoo.com>
2021-06-19 11:22:05 -0600

    JAVASCRIPT TODO APP CHALLENGE

--- todo_app_main/JS/todo.js
+++ todo_app_main/JS/todo.js
@@ -1,12 +1,12 @@
 function get_todos() {
     /**/
     var todos = new Array;
-
     var todos_str = localStorage.getItem('todo');
 
     if (todos_str != null) {
         todos = JSON.parse(todos_str);
     }
+
     return todos;
 }
 
@@ -15,48 +15,46 @@ function add() {
     var task = document.getElementById('task').value;
 
     var todos = get_todos();
-
     todos.push(task);
 
     localStorage.setItem('todo', JSON.stringify(todos));
-    document.getElementById('task').value = "";
-    show();
+    document.getElementById('task').value = '';
 
+    show();
     return false;
 }
 
 function remove() {
+    console.log('Start removing...');
+    console.log('getAttribute:' + this.addEventListener);
     /**/
-    var id = this.getAttribute('id');
+    var btn_id = this.getAttribute('id');
     var todos = get_todos();
-    todos.splice(id, 1);
+    todos.splice(btn_id, 1);
     localStorage.setItem('todo', JSON.stringify(todos));
     //
     show();
-
     return false;
 }
 
 function show() {
+    console.log('Start show()...');
     /**/
     var todos = get_todos();
-
-    var html = '<ul>';
+    var html = '<ul class="todo_list">';
 
     for (let i = 0; i < todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">X</button></li>';
-    }
-
-    var buttons = document.getElementsByClassName('remove');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', remove);
+        html += '<li>' + (i + 1) + ' ) ' + todos[i] + '<button class="remove" id="' + i + '">X</button></li>';
     }
 
     html += '</ul>';
 
     document.getElementById('todos').innerHTML = html;
 
-
+    var buttons = document.getElementsByClassName("remove");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    }
 }
 
 /**/
